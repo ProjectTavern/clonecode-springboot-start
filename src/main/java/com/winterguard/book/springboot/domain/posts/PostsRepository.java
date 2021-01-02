c1002b658f463ec1dbd47986a680431783522c6d
winterguard <aliasbonfire@gmail.com>
2021-01-02 23:18:49 +0900

    조회 API 구현

--- src/main/java/com/winterguard/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/winterguard/book/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,16 @@
 package com.winterguard.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long>{
+
+    /**
+     * Hint: Many companies usually use Querydsl.
+     * Link: http://www.querydsl.com
+     * */
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/winterguard/book/springboot/web/IndexController.java
+++ src/main/java/com/winterguard/book/springboot/web/IndexController.java
@@ -1,13 +1,22 @@
 package com.winterguard.book.springboot.web;
 
+import com.winterguard.book.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.h2.engine.Mode;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.ui.Model;
+
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
