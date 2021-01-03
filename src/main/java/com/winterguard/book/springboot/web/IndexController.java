69c4fb20264ac71396cdb9ef32a2b7571bf68d07
winterguard <aliasbonfire@gmail.com>
2021-01-03 22:34:39 +0900

    로그인 화면 구현

--- src/main/java/com/winterguard/book/springboot/web/IndexController.java
+++ src/main/java/com/winterguard/book/springboot/web/IndexController.java
@@ -1,24 +1,34 @@
 package com.winterguard.book.springboot.web;
 
+import com.winterguard.book.springboot.config.auth.dto.SessionUser;
 import com.winterguard.book.springboot.service.posts.PostsService;
 import com.winterguard.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.h2.engine.Mode;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
