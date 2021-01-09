04e47562dd64a1b4479152e1e4185d66e0913fcb
winterguard <aliasbonfire@gmail.com>
2021-01-09 21:22:44 +0900

    Fixed: could run Test code

--- src/test/java/com/winterguard/book/springboot/web/api/PostsApiControllerTest.java
+++ src/test/java/com/winterguard/book/springboot/web/api/PostsApiControllerTest.java
@@ -1,13 +1,26 @@
 package com.winterguard.book.springboot.web.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.winterguard.book.springboot.domain.posts.Posts;
 import com.winterguard.book.springboot.domain.posts.PostsRepository;
 import com.winterguard.book.springboot.web.dto.PostsSaveRequestDto;
 import com.winterguard.book.springboot.web.dto.PostsUpdateRequestDto;
-import org.apache.coyote.Response;
+
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -35,12 +48,26 @@ public class PostsApiControllerTest {
     @Autowired
     private PostsRepository postsRepository;
 
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
     @After
     public void tearDown() throws Exception {
         postsRepository.deleteAll();
     }
 
     @Test
+    @WithMockUser(roles="USER")
     public void Posts_등록된다() throws Exception {
         // Given
         String title = "title";
@@ -57,12 +84,12 @@ public class PostsApiControllerTest {
         String url = "http://localhost:" + port + "/api/v1/posts";
 
         // When
-        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+        mvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
 
         // Then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
-
         List<Posts> allPosts = postsRepository.findAll();
         assertThat(allPosts.get(0).getTitle()).isEqualTo(title);
         assertThat(allPosts.get(0).getContent()).isEqualTo(content);
